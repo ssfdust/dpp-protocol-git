489caa4ea7359e5d42ce3d182ae40fb06f2430ca
飘尘 <ssfdust@gmail.com>
2024-12-28 17:41:07 +0800

    Compatible with acceleration urls
    In China, due to network issues, users have to use acceleration sites, such as gitclone.com. However, acceleration sites are not mirror sites, and their usage is slightly different. For example, if I need to download dpp.vim from gitclone.com, I have to use the following command:
    git clone https://gitclone.com/https://github.com/Shougo/dpp.vim.git
    
    This results in the final folder path becoming:
    gitclone.com/https///github.com/Shougo/dpp.vim
    
    This patch is intended to accommodate this situation, ultimately generating:
    
    gitclone.com/github.com/Shougo/dpp.vim

--- denops/@dpp-protocols/git.ts
+++ denops/@dpp-protocols/git.ts
@@ -77,7 +77,7 @@ export class Protocol extends BaseProtocol<Params> {
     }
 
     const directory = url.replace(/\.git$/, "").replace(/^https:\/+|^git@/, "")
-      .replace(/:/, "/");
+      .replace(/\/htpps:\/+/, "/").replace(/:/, "/");
 
     return {
       path: `${await vars.g.get(
